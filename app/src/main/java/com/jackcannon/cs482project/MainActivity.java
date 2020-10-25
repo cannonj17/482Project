cbbb925576cdc633676285527caed8ce35206f93
Jack Cannon <CannonJack17@gmail.com>
2020-10-25 17:31:13 -0400

    create new app

---
+++ app/src/main/java/com/jackcannon/cs482project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jackcannon.cs482project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
